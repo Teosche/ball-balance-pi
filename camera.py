9f3efd7d85a263997149fec217740ca561611806
Matteo Jacopo Schembri <matteojacopo.schembri@gmail.com>
2024-12-17 17:50:38 +0100

    fix: update camera.py

--- camera.py
+++ camera.py
@@ -14,37 +14,41 @@ class Camera:
         self.camera.configure(self.camera_config)
         self.camera.start()
 
-    def get_frame(self):
-        """
-        Frame capture.
-        """
-        frame = self.camera.capture_array()
-        text = "Streaming video"
-        cv2.putText(frame, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-        # filters
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        blurFrame = cv2.GaussianBlur(gray, (17, 17), 0)
+def get_frame(self):
+    """
+    Frame capture.
+    """
+    frame = self.camera.capture_array()
+    text = "Streaming video"
+    cv2.putText(frame, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-        # init variablesto neet fix
-        global x
-        global y
-        global speed
-        lock = True
-        previous_x = 10
-        previous_y = 120
+    # Filters
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    blurFrame = cv2.GaussianBlur(gray, (17, 17), 0)
 
-        # define circle contours
-        circles = cv2.HoughCircles(
-            blurFrame,
-            cv2.HOUGH_GRADIENT,
-            1.2,
-            100000,
-            param1=100,
-            param2=30,
-            minRadius=2,
-            maxRadius=40,
-        )
+    # Init variables
+    global x
+    global y
+    global speed
+    lock = True
+    previous_x = 10
+    previous_y = 120
+
+    # Define circle contours
+    circles = cv2.HoughCircles(
+        blurFrame,
+        cv2.HOUGH_GRADIENT,
+        1.2,
+        100000,
+        param1=100,
+        param2=30,
+        minRadius=2,
+        maxRadius=40,
+    )
+
+    # Check if circles were detected
+    if circles is not None:
         circles = np.round(circles[0, :]).astype("int")
 
         for x, y, r in circles:
@@ -52,6 +56,7 @@ class Camera:
             cv2.circle(frame, (x, y), 2, (0, 0, 255), 3)
             x = round(x / 2)
             y = round(y / 2)
+
             if lock is True:
                 previous_x = x
                 previous_y = y
@@ -61,10 +66,14 @@ class Camera:
             speed = round(module, 1)
             print(f"Position: (X:{x}: Y:{y})")
             print(speed)
-            previous_x = round(x / 2)
-            previous_y = round(y / 2)
+            previous_x = x
+            previous_y = y
 
             time.sleep(0.1)
+    else:
+        print("No circles detected.")
+        speed = 0
 
-        _, buffer = cv2.imencode(".jpg", frame)
-        return buffer.tobytes()
+    # Encode the frame to JPEG and return
+    _, buffer = cv2.imencode(".jpg", frame)
+    return buffer.tobytes()
