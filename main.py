e7656c723c8ec750f89d2f57423bc3e6ea6d9754
Matteo Jacopo Schembri <matteojacopo.schembri@gmail.com>
2025-02-18 18:49:34 +0100

    fix: test parameters

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ if __name__ == "__main__":
     pid = PID(kp=0.05, ki=0.0025, kd=0.025, setpoint=(0, 0))
     servo = Servo(pi)
 
-    # stop_event = threading.Event()
-    # vision_thread = threading.Thread(
-    #     target=vision, args=(stop_event, camera, pid, servo)
-    # )
-    # vision_thread.start()
+    stop_event = threading.Event()
+    vision_thread = threading.Thread(
+        target=vision, args=(stop_event, camera, pid, servo)
+    )
+    vision_thread.start()
 
     threading.Thread(
         target=app.run,
