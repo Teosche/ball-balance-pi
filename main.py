df16ac11aa6137cc5204c1e6def9250a56b4ac2e
Matteo Jacopo Schembri <matteojacopo.schembri@gmail.com>
2025-02-20 00:35:22 +0100

    fix: parameter

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     pi = pigpio.pi()
     camera = Camera()
     init_camera(camera)  # Inizializza la variabile globale 'camera' in api.py
-    pid = PID(kp=0.03, ki=0.001, kd=0.01, setpoint=(0, 0))
+    pid = PID(kp=0.03, ki=0.002, kd=0.01, setpoint=(0, 0))
 
     servo = Servo(pi)
 
